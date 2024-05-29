64a4194518bcc416eb79ad41e2666f3e820f5fd9
Esther <esther.zirkler@gmail.com>
2024-05-29 12:06:46 +0200

    03 / HoneyBee abstract

--- com/cc/java/Drone.java
+++ com/cc/java/Drone.java
@@ -2,8 +2,20 @@ package com.cc.java;
 
 public class Drone extends HoneyBee{
 
-    public String doYourJob(){
-        return "I'm a drone, I'm going to date our Queen!";
+    @Override
+    public String doYourJob() {
+         return "I'm a drone, I'm going to date our Queen!";
     }
+
+    @Override
+    public String fly() {
+        return "I can fly at 10mph!";
+    }
+
+    
+
+    // public String doYourJob(){
+    //     return "I'm a drone, I'm going to date our Queen!";
+    // }
 
 }
